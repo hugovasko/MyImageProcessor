c3fe59e7cd9d7b733de7ef9c7aa0cf07a642a462
Vasko Nikolov <vasko.nikolov@reachupbg1.onmicrosoft.com>
2023-08-22 14:16:34 +0300

    main: Add try catch

--- Program.cs
+++ Program.cs
@@ -10,93 +10,103 @@ using SixLabors.ImageSharp.Formats.Jpeg;
 using System.Text;
 using System.Threading.Tasks;
 
-Console.Write("Enter the path to the photos directory: ");
-string imageDirectory = Console.ReadLine() ?? string.Empty;
-
-Console.Write("Enter the path to the output directory: ");
-string outputDirectory = Console.ReadLine() ?? string.Empty;
-
-Console.Write("Enter the path to the text dictionary file: ");
-string textFilePath = Console.ReadLine() ?? string.Empty;
-
-if (!Directory.Exists(imageDirectory) || !Directory.Exists(outputDirectory) || !File.Exists(textFilePath))
+try
 {
-    Console.WriteLine("One or more paths are incorrect. Please check and try again.");
-    return;
-}
 
-Console.WriteLine("Processing images...");
+    Console.Write("Enter the path to the photos directory: ");
+    string imageDirectory = Console.ReadLine() ?? string.Empty;
 
-var texts = new Dictionary<string, string>();
-string[] lines = File.ReadAllLines(textFilePath);
-foreach (string line in lines)
-{
-    string[] parts = line.Split(new[] { ": " }, StringSplitOptions.None);
-    if (parts.Length == 2)
+    Console.Write("Enter the path to the output directory: ");
+    string outputDirectory = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("Enter the path to the text dictionary file: ");
+    string textFilePath = Console.ReadLine() ?? string.Empty;
+
+    if (!Directory.Exists(imageDirectory) || !Directory.Exists(outputDirectory) || !File.Exists(textFilePath))
     {
-        texts.Add(parts[0], parts[1]);
+        Console.WriteLine("One or more paths are incorrect. Please check and try again.");
+        return;
     }
-}
 
-// Define horizontal A4 size in pixels (assuming 300 DPI)
-Size a4Size = new Size(3508, 2480);
+    Console.WriteLine("Processing images...");
 
-Parallel.ForEach(texts, item =>
-{
-    string imagePath = System.IO.Path.Combine(imageDirectory, item.Key);
-    string text = item.Value;
-
-    using (Image<Rgba32> image = Image.Load<Rgba32>(imagePath))
+    var texts = new Dictionary<string, string>();
+    string[] lines = File.ReadAllLines(textFilePath);
+    foreach (string line in lines)
     {
-        int dpi = 300;
-        float baseFontSize = 25; // Base font size for 150 DPI
-        float scaleFactor = dpi / 150f;
-        float calculatedFontSize = baseFontSize * scaleFactor;
-
-        var font = SystemFonts.CreateFont("Arial", calculatedFontSize);
-
-        // Wrap the text and measure its size
-        string wrappedText = WrapText(text, font, a4Size.Width - 50);
-        FontRectangle wrappedTextSize = TextMeasurer.MeasureBounds(wrappedText, new RendererOptions(font));
-
-        // Determine the available height for the image
-        int availableImageHeight = a4Size.Height - (int)wrappedTextSize.Height - 75; // 75 for margins
-        Size newSize = new Size(a4Size.Width, availableImageHeight);
-
-        using (Image<Rgba32> a4Image = new Image<Rgba32>(Configuration.Default, a4Size.Width, a4Size.Height, Color.White))
+        string[] parts = line.Split(new[] { ": " }, StringSplitOptions.None);
+        if (parts.Length == 2)
         {
-            a4Image.Mutate(ctx =>
-            {
-                ctx.DrawImage(image.Clone(ic => ic.Resize(newSize)), new Point(0, 0), 1);
-
-                // Wrap the text manually
-                var wrappedTextLines = WrapText(text, font, a4Size.Width - 50).Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
-                // Draw each line, centering it horizontally
-                float textY = newSize.Height + 25;
-                foreach (var line in wrappedTextLines)
-                {
-                    var textSize = TextMeasurer.MeasureSize(line, new RendererOptions(font));
-                    float textX = (a4Size.Width - textSize.Width) / 2;
-
-                    ctx.DrawText(line, font, Color.Black, new PointF(textX, textY));
-                    textY += textSize.Height; // Move to the next line
-                }
-            });
-
-            string outputPath = System.IO.Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(item.Key) + ".jpg");
-
-            var options = new JpegEncoder
-            {
-                Quality = 85
-            };
-
-            a4Image.Save(outputPath, options);
+            texts.Add(parts[0], parts[1]);
         }
     }
-});
 
-Console.WriteLine("All images processed!");
+    // Define horizontal A4 size in pixels (assuming 300 DPI)
+    Size a4Size = new Size(3508, 2480);
+
+    Parallel.ForEach(texts, item =>
+    {
+        string imagePath = System.IO.Path.Combine(imageDirectory, item.Key);
+        string text = item.Value;
+
+        using (Image<Rgba32> image = Image.Load<Rgba32>(imagePath))
+        {
+            int dpi = 300;
+            float baseFontSize = 25; // Base font size for 150 DPI
+            float scaleFactor = dpi / 150f;
+            float calculatedFontSize = baseFontSize * scaleFactor;
+
+            var font = SystemFonts.CreateFont("Arial", calculatedFontSize);
+
+            // Wrap the text and measure its size
+            string wrappedText = WrapText(text, font, a4Size.Width - 50);
+            FontRectangle wrappedTextSize = TextMeasurer.MeasureBounds(wrappedText, new RendererOptions(font));
+
+            // Determine the available height for the image
+            int availableImageHeight = a4Size.Height - (int)wrappedTextSize.Height - 75; // 75 for margins
+            Size newSize = new Size(a4Size.Width, availableImageHeight);
+
+            using (Image<Rgba32> a4Image = new Image<Rgba32>(Configuration.Default, a4Size.Width, a4Size.Height, Color.White))
+            {
+                a4Image.Mutate(ctx =>
+                {
+                    ctx.DrawImage(image.Clone(ic => ic.Resize(newSize)), new Point(0, 0), 1);
+
+                    // Wrap the text manually
+                    var wrappedTextLines = WrapText(text, font, a4Size.Width - 50).Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                    // Draw each line, centering it horizontally
+                    float textY = newSize.Height + 25;
+                    foreach (var line in wrappedTextLines)
+                    {
+                        var textSize = TextMeasurer.MeasureSize(line, new RendererOptions(font));
+                        float textX = (a4Size.Width - textSize.Width) / 2;
+
+                        ctx.DrawText(line, font, Color.Black, new PointF(textX, textY));
+                        textY += textSize.Height; // Move to the next line
+                    }
+                });
+
+                string outputPath = System.IO.Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(item.Key) + ".jpg");
+
+                var options = new JpegEncoder
+                {
+                    Quality = 85
+                };
+
+                a4Image.Save(outputPath, options);
+            }
+        }
+    });
+
+    Console.WriteLine("All images processed!");
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 static string WrapText(string text, Font font, float maxWidth)
 {
